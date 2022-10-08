17684225d988ed4779ed7c05701d7ff06094c2a9
SaxenaBhoomika <89317642+SaxenaBhoomika@users.noreply.github.com>
2022-10-08 19:23:53 +0530

    Uploaded basic JS file

---
+++ words.js
@@ -0,0 +1,57 @@
+let words = [
+    {
+        word: "webbtelescope",
+        hint: "World's largest, most powerful, and most complex space science telescope ever built"
+    },
+    {
+        word: "infrared",
+        hint: "These radiations are captured by the JWST only"
+    },
+    {
+        word: "hydrazine",
+        hint: "Fuel used by JWST"
+    },
+    {
+        word: "carbondioxide",
+        hint: "The first clear evidence of presence of this gas outside the solar system was identified by JSWT "
+    },
+    {
+        word: "december",
+        hint: "JSWT was launched in this month"
+    },
+    {
+        word: "hubble",
+        hint: "JSWT is atleast 100 times more powerful than this telescope"
+    },
+    {
+        word: "redshifting",
+        hint: "A process wherein light is stretched as it appears and disappears, leaving a red trail behind"
+    },
+    {
+        word: "silicon",
+        hint: "This element is coated on the outermost sun-facing layers of JSWT"
+    },
+    {
+        word: "Fowler",
+        hint: "He helped to make the James Webb mirrors"
+    },
+    {
+        word: "golden",
+        hint: "The James Webb mirrors are coated in this color"
+    },
+    {
+        word: "beryllium",
+        hint: "The primary mirrors of JSWT is made of this element"
+    },
+    {
+        word: "eight",
+        hint: "Number of planets in solar system"
+    },
+    {
+        word: "earth",
+        hint: "Called as blue planet"
+    },
+    {
+        word: "solar",
+        hint: "Sun is the center of this system"
+    }]
