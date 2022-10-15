b806fc66ddccdc89b97fff3adc6feb3c20ead543
Aryan Sharma <aryansharma2909@gmail.com>
2022-10-15 23:14:17 +0530

    added quiz game web

---
+++ web/quiz-game/js/questions.js
@@ -0,0 +1,79 @@
+let MCQS = [{
+    question: "JWST stands for ?",
+    choice1: "james Webb Space Telescope",
+    choice2: "John Webb Space Telescope",
+    choice3: "Jim Webb Space Telescope",
+    choice4: "None",
+    answer: 0
+},
+            {
+            question: "JWST is launched on ?",
+    choice1: "20th December 2021",
+    choice2: "20th November 2021",
+    choice3: "25th December 2021",
+    choice4: "25th November 2021",
+    answer: 3
+},
+            {
+            question: "Where JWST is placed in space?",
+    choice1: "Lagrange 1",
+    choice2: "Lagrange 2 ",
+    choice3: "Lagrange 3",
+    choice4: "Lagrange 4",
+    answer: 1
+},
+            {
+            question: "How many mirrors are used in JWST ?",
+    choice1: "18",
+    choice2: "16",
+    choice3: "14",
+    choice4: "12",
+    answer: 0
+},
+            {
+            question: "What is the shape of mirrors ?",
+    choice1: "Square",
+    choice2: "Circle",
+    choice3: "Pentagon",
+    choice4: "Hexagon",
+    answer: 3
+},
+            {
+            question: " Which sensors are used in capturing images ?",
+    choice1: "Guidance Sensor",
+    choice2: "Near Infrared Imager and Slitless Spectrograph",
+    choice3: "UV sensor",
+    choice4: "None",
+    answer: 1
+},
+            {
+               question: "How many layers of sunshield are used to protect the mirrors?",
+    choice1: "3",
+    choice2: "4",
+    choice3: "5",
+    choice4: "6",
+    answer: 2
+},{
+               question: " What is the telescope’s primary mirror material?",
+    choice1: "Beryllium coated with Gold",
+    choice2: " Aluminum",
+    choice3: "Metal",
+    choice4: "Iron",
+    answer: 0
+},
+            {
+               question: "What types of light spectrum JWST can detect?",
+    choice1: "Visible light",
+    choice2: "IR rays",
+    choice3: " Microwaves",
+    choice4: "UV rays",
+    answer: 1
+},
+            {
+               question: "10. What is the mirror diameter of JWST?",
+    choice1: "6.9m",
+    choice2: "7.9m",
+    choice3: "6,3m",
+    choice4: "6.5m",
+    answer: 3
+}];
